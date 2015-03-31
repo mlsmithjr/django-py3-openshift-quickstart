aea87b0c51af89e5bb35391d9c32e7c8b5c2b7b1
Mark Smith <masmith@redhat.com>
2015-03-30 22:26:39 -0400

    updated to django 1.7.7

--- setup.py
+++ setup.py
@@ -5,16 +5,12 @@ import os
 # Put here required packages or
 # Uncomment one or more lines below in the install_requires section
 # for the specific client drivers/modules your application needs.
-packages = ['Django<=1.6',
+packages = ['Django<=1.7.7',
             'static3',  # If you want serve the static files in the same server
             # 'mysql-connector-python',
             # 'pymongo',
             # 'psycopg2',
            ]
-
-if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
-     packages.append('django-redis-cache')
-     packages.append('hiredis')
 
 setup(name='YourAppName', version='1.0',
       description='OpenShift Python-3.3 / Django-1.6 Community Cartridge based application',
